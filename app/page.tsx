5ec453d9e5f95f58cd5d1d9f3cc73a350acbac72
SiddheshSomvanshi01 <siddhesh.incronix@gmail.com>
2024-09-17 11:19:08 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,9 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <ContainerScroll titleComponent={""} children={<Hero />} />
+        <ContainerScroll titleComponent="">
+          <Hero />
+        </ContainerScroll>
         <Grid />
         <About />
         <Timeline data={timelineData} />
